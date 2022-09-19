244ab780d31e33011f21e5a1113a50d026e764e4
andrew-matt <andrew.matt@ya.ru>
2022-09-19 15:39:54 +0300

    post and delete requests

--- src/app/components/todos/todos/todos.component.ts
+++ src/app/components/todos/todos/todos.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { apiKey } from '../../../../environments/api-key.environment'
 
 interface Todo {
   addedDate: string
   id: string
   order: number
   title: string
+}
+
+interface BaseResponse<T = {}> {
+  data: T
+  messages: string[]
+  fieldsErrors: string[]
+  resultCode: number
 }
 
 @Component({
@@ -15,6 +23,13 @@ interface Todo {
 })
 export class TodosComponent implements OnInit {
   todos: Todo[] = []
+  baseURL = 'https://social-network.samuraijs.com/api/1.1/'
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': apiKey,
+    },
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -23,12 +38,28 @@ export class TodosComponent implements OnInit {
   }
 
   getTodos() {
+    this.http.get<Todo[]>(`${this.baseURL}todo-lists`, this.httpOptions).subscribe(res => {
+      this.todos = res
+    })
+  }
+
+  createTodo() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    const title = 'Angular' + randomNumber
     this.http
-      .get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-        withCredentials: true,
-      })
+      .post<BaseResponse<{ item: Todo }>>(`${this.baseURL}todo-lists`, { title }, this.httpOptions)
       .subscribe(res => {
-        this.todos = res
+        const newTodo = res.data.item
+        this.todos.unshift(newTodo)
+      })
+  }
+
+  deleteTodo() {
+    const todoId = '254352c7-dc8f-410a-ab81-2f4c550ddea9'
+    this.http
+      .delete<BaseResponse>(`${this.baseURL}todo-lists/${todoId}`, this.httpOptions)
+      .subscribe(res => {
+        this.todos = this.todos.filter(todolist => todolist.id !== todoId)
       })
   }
 }
