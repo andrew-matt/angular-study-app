54c71dedf7bbb79f790086cc58ffc753345aa199
andrew-matt <andrew.matt@ya.ru>
2022-09-23 12:56:34 +0300

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { TodosComponent } from './components/todos/todos/todos.component'
-import { HttpClientModule } from '@angular/common/http'
-import { LoginComponent } from './components/login/login/login.component'
+import { FormsModule } from '@angular/forms'
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component'
+import { TodosComponent } from './components/todos/todos.component'
+import { UsersComponent } from './components/users/users.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    TodosComponent,
+    UsersComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'todos', component: TodosComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'profile', component: ProfileComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
